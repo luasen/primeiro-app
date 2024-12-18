c2d7adbe0ebc7ca18d12aaab2671d4a6aaca9940
luasen <luansena.0100@gmail.com>
2024-12-18 20:20:50 -0300

    mudança inicial

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let valor = document.querySelector(".preco")
+
+    function salvar(){
+      
+     
+    }
+    function adicionarNome()
+     { let nome = document.getElementById("nome").value;
+         document.getElementById("nome-exibido").textContent = nome; }
+
+
+         
+
+        function salvar2(){
+            let titulo = document.querySelector(".nome").value
+document.querySelector(".nome22").textContent = titulo
+
+         }
